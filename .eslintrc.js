212bb126b2f991d1a9943bbebe43b77e7fa004a9
PachyuChepe <pachyuchepe@gmail.com>
2024-04-01 16:23:13 +0900

    [Feat] dev: ESLine & Prettierrc 설정
    - Google 컨벤션 사용

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,10 +5,15 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: [
+    '@typescript-eslint/eslint-plugin',
+    'prettier', // Prettier 플러그인 추가
+  ],
   extends: [
     'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
+    'plugin:prettier/recommended', // Prettier 권장 설정 추가
+    // 'prettier/@typescript-eslint', // TypeScript와 Prettier 충돌 방지 설정
+    'google', // Google 스타일 가이드 추가
   ],
   root: true,
   env: {
@@ -22,5 +27,18 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'object-curly-spacing': 'off', // 중괄호 공백 관련 규칙 비활성화
+    'require-jsdoc': 'off', // JSDoc 관련 규칙 비활성화
+    'indent': 'off', // 들여쓰기 관련 규칙 비활성화
+    'new-cap': 'off',
+    'max-len': 'off', // max-len 규칙 비활성화
+    'prettier/prettier': [
+      'error',
+      {},
+      {
+        usePrettierrc: true,
+      },
+    ],
+    // 필요에 따라 추가적인 ESLint 규칙을 정의
   },
 };
