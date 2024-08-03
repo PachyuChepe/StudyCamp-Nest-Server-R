5eb7781938495c77495786c6e90dffb88f3168f7
PachyuChepe <pachyuchepe@gmail.com>
2024-08-03 15:06:19 +0900

    [Feat] dev: 프로젝트 초기 설정 & 라이브러리 최신화

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,54 @@
+// src/app.module.ts
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { validationSchema } from './config/validation.schema';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
+import { addTransactionalDataSource } from 'typeorm-transactional';
+import { DataSource } from 'typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    // ConfigModule을 하여, 환경 변수 파일의 위치와 유효성 검증 스키마를 지정
+    ConfigModule.forRoot({
+      envFilePath: `.env`, // 환경 변수 파일 경로
+      validationSchema, // 환경 변수 유효성 검증을 위한 스키마
+    }),
+
+    // TypeOrmModule을 비동기적으로 설정하며, 데이터베이스 설정은 ConfigService를 통해 동적으로 로드한다
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule], // ConfigModule을 의존성으로 가져오기
+      inject: [ConfigService], // ConfigService를 주입하여 환경 변수에 접근
+
+      // 데이터베이스 설정을 동적으로 생성하는 팩토리 함수
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres', // 데이터베이스 유형 PostgreSQL
+        host: configService.get<string>('DB_HOST'), // 데이터베이스 호스트
+        port: configService.get<number>('DB_PORT'), // 데이터베이스 포트
+        username: configService.get<string>('DB_USERNAME'), // 데이터베이스 사용자 이름
+        password: configService.get<string>('DB_PASSWORD'), // 데이터베이스 비밀번호
+        database: configService.get<string>('DB_NAME'), // 데이터베이스 이름
+        autoLoadEntities: true, // 엔티티 자동 로드
+        synchronize: configService.get<string>('SERVER_RUNTIME') !== 'prod', // 비프로덕션 환경에서 DB 스키마 자동 동기화
+        logging: configService.get<string>('SERVER_RUNTIME') !== 'prod', // 비프로덕션 환경에서 로깅 활성화
+      }),
+
+      // 데이터 소스 팩토리를 비동기로 정의하며, TypeORM의 DataSource 인스턴스를 생성하고 트랜잭션 기능을 추가한다
+      async dataSourceFactory(options) {
+        if (!options) {
+          throw new Error('Invalid options passed'); // 옵션이 유효하지 않을 때 오류 처리
+        }
+        return addTransactionalDataSource(new DataSource(options)); // 트랜잭셔널 데이터 소스 추가
+      },
+    }),
+    AuthModule, // 인증 모듈 임포트
+  ],
+  // controllers 배열은 이 모듈에 포함될 컨트롤러를 명시
   controllers: [AppController],
+
+  // providers 배열은 이 모듈에서 사용할 서비스(Provider)를 명시
   providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,61 @@
+// src/main.ts
+import * as fs from 'fs';
+import * as https from 'https';
+import * as path from 'path';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { corsOption, getNestOptions } from './app.options';
+import { ConfigService } from '@nestjs/config';
+import { initializeTransactionalContext } from 'typeorm-transactional';
+import { BusinessExceptionFilter } from './exception';
 
+// NestJS 애플리케이션을 설정하고 시작하기 위해 'bootstrap'이라는 비동기 함수를 정의
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  // 데이터베이스 작업에서 트랜잭션의 원자성을 보장하기 위해 트랜잭셔널 컨텍스트를 초기화
+  initializeTransactionalContext();
+
+  // 특정 옵션으로 새로운 NestJS 애플리케이션 인스턴스를 생성
+  const app = await NestFactory.create(AppModule, getNestOptions());
+  // 전체 애플리케이션에서 특정 비즈니스 예외를 처리하기 위해 전역 예외 필터를 적용
+  app.useGlobalFilters(new BusinessExceptionFilter());
+
+  // 환경 또는 기타 외부 소스에서 구성 설정을 검색
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('SERVER_PORT') || 4000; // 지정되지 않은 경우 기본 포트 4000 사용
+  const env = configService.get<string>('SERVER_RUNTIME'); // 서버가 실행되는 환경
+  const serviceName = configService.get<string>('SERVER_SERVICE_NAME'); // 식별을 위한 서비스 이름
+
+  // HTTPS를 위한 SSL 키 및 인증서의 경로 설정
+  const keyPath = path.join(__dirname, '..', 'key.pem');
+  const certPath = path.join(__dirname, '..', 'cert.pem');
+
+  // 환경별 옵션을 사용하여 CORS를 활성화
+  app.enableCors(corsOption(env));
+
+  // SSL 키와 인증서 파일이 있는지 확인
+  if (fs.existsSync(keyPath) && fs.existsSync(certPath)) {
+    // SSL 키와 인증서 파일을 읽음
+    const httpsOptions = {
+      key: fs.readFileSync(keyPath),
+      cert: fs.readFileSync(certPath),
+    };
+
+    // 애플리케이션 경로 및 기타 설정을 초기화
+    await app.init();
+    // SSL 옵션과 NestJS 애플리케이션을 요청 핸들러로 사용하여 HTTPS 서버를 생성
+    https
+      .createServer(httpsOptions, app.getHttpAdapter().getInstance())
+      .listen(port);
+    console.log(
+      `HTTPS server running on\nruntime: ${env}\nport: ${port}\nserviceName: ${serviceName}`,
+    );
+  } else {
+    // SSL 파일을 찾지 못한 경우 애플리케이션을 HTTP로 시작
+    await app.listen(port);
+    console.log(
+      `HTTP server running on\nruntime: ${env}\nport: ${port}\nserviceName: ${serviceName}`,
+    );
+  }
 }
+
 bootstrap();
