503791d29cdd7d33cc4d1f43edd47559d63c272c
PachyuChepe <pachyuchepe@gmail.com>
2024-08-03 18:16:36 +0900

    트랜잭셔널 컨텍스트 초기화, 글로벌 예외 필터 설정, CORS 구성, HTTPS 서버 설정 및 HTTP로 대체
    - 데이터베이스 작업을 위한 트랜잭셔널 컨텍스트 초기화
    - 특정 옵션으로 NestJS 애플리케이션 생성
    - 글로벌 비즈니스 예외 필터 적용
    - ConfigService에서 구성 설정 가져오기
    - SSL 키와 인증서 경로 설정
    - API 문서를 위한 Swagger 통합
    - 환경별 옵션으로 CORS 활성화
    - SSL 키와 인증서 파일을 확인하여 HTTPS 서버 시작 또는 HTTP로 대체
    - 서버 런타임, 포트 및 서비스 이름 로깅

---
+++ src/main.ts
@@ -0,0 +1,64 @@
+// src/main.ts
+import * as fs from 'fs';
+import * as https from 'https';
+import * as path from 'path';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { corsOption, getNestOptions } from './app.options';
+import { ConfigService } from '@nestjs/config';
+import { initializeTransactionalContext } from 'typeorm-transactional';
+import { setSwagger } from './app.swagger';
+import { BusinessExceptionFilter } from './exception';
+
+async function bootstrap() {
+  // 데이터베이스 작업에서 트랜잭션의 원자성을 보장하기 위해 트랜잭셔널 컨텍스트를 초기화
+  initializeTransactionalContext();
+
+  // 특정 옵션으로 새로운 NestJS 애플리케이션 인스턴스를 생성
+  const app = await NestFactory.create(AppModule, getNestOptions());
+  // 전체 애플리케이션에서 특정 비즈니스 예외를 처리하기 위해 전역 예외 필터를 적용
+  app.useGlobalFilters(new BusinessExceptionFilter());
+
+  // 환경 또는 기타 외부 소스에서 구성 설정을 검색
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('SERVER_PORT') || 4000;
+  const env = configService.get<string>('SERVER_RUNTIME');
+  const serviceName = configService.get<string>('SERVER_SERVICE_NAME');
+
+  // HTTPS를 위한 SSL 키 및 인증서의 경로 설정
+  const keyPath = path.join(__dirname, '..', 'key.pem');
+  const certPath = path.join(__dirname, '..', 'cert.pem');
+
+  // Swagger API 연결
+  setSwagger(app);
+
+  // 환경별 옵션을 사용하여 CORS를 활성화
+  app.enableCors(corsOption(env));
+
+  // SSL 키와 인증서 파일이 있는지 확인
+  if (fs.existsSync(keyPath) && fs.existsSync(certPath)) {
+    // SSL 키와 인증서 파일을 읽음
+    const httpsOptions = {
+      key: fs.readFileSync(keyPath),
+      cert: fs.readFileSync(certPath),
+    };
+
+    // 애플리케이션 경로 및 기타 설정을 초기화
+    await app.init();
+    // SSL 옵션과 NestJS 애플리케이션을 요청 핸들러로 사용하여 HTTPS 서버를 생성
+    https
+      .createServer(httpsOptions, app.getHttpAdapter().getInstance())
+      .listen(port);
+    console.log(
+      `✅ HTTPS server running on\n✅ runtime: ${env}\n✅ port: ${port}\n✅ serviceName: ${serviceName}`,
+    );
+  } else {
+    // SSL 파일을 찾지 못한 경우 애플리케이션을 HTTP로 시작
+    await app.listen(port);
+    console.log(
+      `✅ HTTP server running on\n✅ runtime: ${env}\n✅ port: ${port}\n✅ serviceName: ${serviceName}`,
+    );
+  }
+}
+
+bootstrap();
